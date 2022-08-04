changed result layout, and fixed underline p-value calculation.

streamlit_app.py
@@ -4,6 +4,8 @@ import numpy as np
 import scipy.stats
 from scipy.stats import norm
 import altair as alt
+import matplotlib.pyplot as plt
+
 
 st.set_page_config(
     page_title="A/B Testing App", page_icon="📊", initial_sidebar_state="expanded"
@@ -26,18 +28,24 @@ def std_err_diff(sea, seb):
     return np.sqrt(sea ** 2 + seb ** 2)
 
 
-def z_score(cra, crb, error):
-    return ((crb - cra) / error) / 100
-
+def z_score(conversions_a, conversions_b, visitors_a, visitors_b):
+    cr = (conversions_a + conversions_b) / (visitors_a + visitors_b)
+    cra = conversions_a / visitors_a
+    crb = conversions_b / visitors_b
+    return (crb - cra)/np.sqrt(cr*(1-cr)*(1/visitors_a+1/visitors_b))
 
 def p_value(z, hypothesis):
-    if hypothesis == "One-sided" and z < 0:
-        return 1 - norm().sf(z)
-    elif hypothesis == "One-sided" and z >= 0:
-        return norm().sf(z)
+    if hypothesis == "One-sided":
+        if z < 0:
+            p_value = 1 - norm().sf(z)
+        if z >= 0:
+            p_value = norm().sf(z)
     else:
-        return norm().sf(z) * 2
-
+        if z < 0:
+            p_value = (1 - norm().sf(z)) * 2
+        if z >= 0:
+            p_value = norm().sf(z) * 2
+    return p_value
 
 def significance(alpha, p):
     return "YES" if p < alpha else "NO"
@@ -104,35 +112,34 @@ def calculate_significance(
     st.session_state.seb = std_err(st.session_state.crb, float(visitors_b))
     st.session_state.sed = std_err_diff(st.session_state.sea, st.session_state.seb)
     st.session_state.z = z_score(
-        st.session_state.cra, st.session_state.crb, st.session_state.sed
+        conversions_a, conversions_b, visitors_a, visitors_b
     )
     st.session_state.p = p_value(st.session_state.z, st.session_state.hypothesis)
     st.session_state.significant = significance(
         st.session_state.alpha, st.session_state.p
     )
 
-st.image("versai.png", width=200)
-
+st.image("versai.png", width=150)
 st.write(
     """
-    # Significance Calculation Tool for VERSAI use only (for now)
+    ## Significance Calculation Tool
     """
 )
 
-st.markdown("### Type in your data")
+st.markdown("### Type in your data (or use the default sample data)")
 #st.write("Type in your data to see whether the difference between two groups is significant")
 
 col1, col2 = st.columns(2)
 
 with col1:
     st.markdown("##### Group A")
-    Pop_A = st.number_input('population counts', value = 228)
-    Evt_A = st.number_input('event counts', value = 23)
+    Pop_A = st.number_input('population count', value = 228)
+    Evt_A = st.number_input('event count', value = 23)
 
 with col2:
     st.markdown("##### Group B")
-    Pop_B = st.number_input('population counts', value=254)
-    Evt_B = st.number_input('event counts', value = 30)
+    Pop_B = st.number_input('population count', value=254)
+    Evt_B = st.number_input('event count', value = 30)
 
 st.markdown("### Adjust test parameters")
 
@@ -175,61 +182,67 @@ mcol1, mcol2 = st.columns(2)
 
 with mcol1:
     st.metric(
-        "Delta",
+        "Difference",
         value=f"{(st.session_state.crb - st.session_state.cra):.3g}%",
     )
 # Display whether or not A/B test result is statistically significant
 with mcol2:
     st.metric("Significant?", value=st.session_state.significant)
 
-st.markdown("### Result Details")
+st.markdown("### Result Explanation")
 
-with st.expander("See explanation"):
-    tab1, tab2 = st.tabs(["Table", "Graph"])
+mcol1, mcol2 = st.columns(2)
 
-    with tab1:
-        table = pd.DataFrame(
-            {
-                "Event": [Evt_A, Evt_B],
-                "Population": [Pop_A, Pop_B],
-                "% events in the population": [st.session_state.cra, st.session_state.crb],
-            },
-            index=pd.Index(["Group A", "Group B"]),
+with mcol1:
+    table = pd.DataFrame(
+        {
+            "Event": [Evt_A, Evt_B],
+            "Population": [Pop_A, Pop_B],
+            "% events": [st.session_state.cra, st.session_state.crb],
+        },
+        index=pd.Index(["Group A", "Group B"]),
+    )
+
+    # Format "% Converted" column values to 3 decimal places
+    table1 = st.write(table.style.format(formatter={("% events"): "{:.3g}%"}))
+
+    metrics = pd.DataFrame(
+        {
+            "diff": [st.session_state.diff],
+            "z-score": [st.session_state.z],
+            "p-value": [st.session_state.p],
+        },
+        index=pd.Index(["Metrics"]),
+    )
+
+    # Color negative values red; color significant p-value green and not significant red
+    table2 = st.write(
+        metrics.style.format(
+            formatter={("p-value", "z-score"): "{:.3g}", ("diff"): "{:.3g}%"}
         )
+            # .applymap(style_negative, props="color:red;")
+            .apply(style_p_value, props="color:red;", axis=1, subset=["p-value"])
+    )
 
-        # Format "% Converted" column values to 3 decimal places
-        table1 = st.write(table.style.format(formatter={("% events in the population"): "{:.3g}%"}))
+with mcol2:
+    labels = ['Group A', 'Group B']
+    x_pos = np.arange(len(labels))
+    cra = [st.session_state.cra, st.session_state.crb]
+    error = [st.session_state.sea*100*1.96, st.session_state.seb*100*1.96]
 
-        metrics = pd.DataFrame(
-            {
-                "p-value": [st.session_state.p],
-                "z-score": [st.session_state.z],
-                "diff": [st.session_state.diff],
-            },
-            index=pd.Index(["Metrics"]),
-        )
+    fig, ax = plt.subplots()
+    ax.bar(x_pos,
+           cra,
+           yerr=error,
+           align='center',
+           alpha=0.5,
+           ecolor='black',
+           capsize=10)
 
-        # Color negative values red; color significant p-value green and not significant red
-        table2 = st.write(
-            metrics.style.format(
-                formatter={("p-value", "z-score"): "{:.3g}", ("diff"): "{:.3g}%"}
-            )
-                .applymap(style_negative, props="color:red;")
-                .apply(style_p_value, props="color:red;", axis=1, subset=["p-value"])
-        )
+    ax.set_ylabel("event %")
+    ax.yaxis.grid(True)
+    ax.set_xticks(x_pos)
+    ax.set_xticklabels(labels)
+    ax.set_title(f"Event % at {(1-st.session_state.alpha)*100}% Confidence Level")
 
-    with tab2:
-        # Create a single-row, two-column DataFrame to use in bar chart
-        results_df = pd.DataFrame(
-            {
-                "Group": ["Group A", "Group B"],
-                "Conversion": [st.session_state.cra, st.session_state.crb],
-            }
-        )
-        st.write("")
-        st.write("")
-
-        # Plot bar chart of conversion rates
-        plot_chart(results_df)
-
-
+    st.pyplot(fig)